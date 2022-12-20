840f805fa2cf7621707da6890d03e3ffedb5ca87
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-20 11:00:02 -0300

    Enum

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using orderSample.Entities;
+using orderSample.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();
+Console.WriteLine(txt);
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+Console.WriteLine(os);
